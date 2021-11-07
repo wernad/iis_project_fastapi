6251a78d454bea302f419ff8b2a7db0839649d06
wernad <jztnem@gmail.com>
2021-11-07 20:06:02 +0100

    add datetime column to some models

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "mysql://admin:iis7319@localhost/fituska"
+SQLALCHEMY_DATABASE_URL = 'mysql://admin:iis7319@localhost/fituska'
 
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 
--- models.py
+++ models.py
@@ -1,5 +1,6 @@
 from sqlalchemy import Boolean, Column, ForeignKey, Integer, String
 from sqlalchemy.orm import relationship
+from sqlalchemy.sql.sqltypes import DateTime
 
 from database import Base
 
@@ -17,23 +18,24 @@ class User(Base):
 
     management_level = Column(Integer)
 
-    questions = relationship('Question', back_populates='author')
-    answers = relationship('Answer', back_populates='author')
-    reactions = relationship('Reaction', back_populates='author')
-    courses = relationship('UserCourse', back_populates='supervisors')
+    questions = relationship('Question', back_populates='user')
+    answers = relationship('Answer', back_populates='user')
+    reactions = relationship('Reaction', back_populates='user')
+    courses = relationship('UserCourse', back_populates='users')
 
 class Course(Base):
     __tablename__ = 'course'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), index=True, nullable=False)
+    approved = Column(Boolean)
 
 class Category(Base):
     __tablename__ = 'category'
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String(50), nullable=False)
-    course = Column(Integer, ForeignKey('course.id'))
+    course_id = Column(Integer, ForeignKey('course.id'))
 
 class Question(Base):
     __tablename__ = 'question'
@@ -44,43 +46,47 @@ class Question(Base):
     is_open = Column(Boolean, nullable=False)
     category = Column(Integer, nullable=False)
     course = Column(Integer, nullable=False)
-    user = Column(Integer, ForeignKey('user.id'))
+    date = Column(DateTime, nullable=False)
 
-    author = relationship('User', back_populates='questions')
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    user = relationship('User', back_populates='questions')
 
 class Answer(Base):
     __tablename__ = 'answer'
     
     id = Column(Integer, primary_key=True, index=True)
     description = Column(String(1000), nullable=False)
-    question = Column(Integer, ForeignKey('question.id'))
-    user = Column(Integer, ForeignKey('user.id'))
+    date = Column(DateTime, nullable=False)
 
-    author = relationship('User', back_populates='answers')
+    question_id = Column(Integer, ForeignKey('question.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    user = relationship('User', back_populates='answers')
 
 class Reaction(Base):
     __tablename__ = 'reaction'
 
     id = Column(Integer, primary_key=True, index=True)
     description = Column(String(1000), nullable=False)
-    answer = Column(Integer, ForeignKey('answer.id'))
-    user = Column(Integer, ForeignKey('user.id'))
+    date = Column(DateTime, nullable=False)
 
-    author = relationship('User', back_populates='reactions')
+    answer_id = Column(Integer, ForeignKey('answer.id'))
+    user_id = Column(Integer, ForeignKey('user.id'))
+
+    user = relationship('User', back_populates='reactions')
 
 class Upvote(Base):
     __tablename__ = 'upvote'
 
-    id = Column(Integer, primary_key=True, index=True)
-    answer = Column(Integer, ForeignKey('answer.id'))
-    user = Column(Integer, ForeignKey('user.id'))
+    answer_id = Column(Integer, ForeignKey('answer.id'), primary_key=True)
+    user_id = Column(Integer, ForeignKey('user.id'), primary_key=True)
 
 class UserCourse(Base):
-    __tablename__ = 'teachercourse'
+    __tablename__ = 'usercourse'
 
     id = Column(Integer, ForeignKey('user.id'), primary_key=True, index=True)
-    course = Column(Integer, ForeignKey('course.id'), primary_key=True, index=True)
+    course_id = Column(Integer, ForeignKey('course.id'), primary_key=True, index=True)
     teacher = Column(Boolean)
-    approved = Column(Boolean)
 
-    supervisors = relationship('User', back_populates='courses')
+    users = relationship('User', back_populates='courses')
