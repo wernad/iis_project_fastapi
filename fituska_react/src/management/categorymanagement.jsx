ada1d4b0ace970e8deb627d1ec1d797e284f7856
wernad <jztnem@gmail.com>
2021-11-29 12:52:43 +0100

    fix unnecessary page reload on update and add of category

--- fituska_react/src/management/categorymanagement.jsx
+++ fituska_react/src/management/categorymanagement.jsx
@@ -1,67 +1,154 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import Cookies from "universal-cookie";
 
 const CategoryManagement = ({teacherCourses}) => {
+    const [coursesCategories, setCoursesCategories] = useState({})
     const [showCategories, setShowCategories] = useState(null);
-    const [errors, setErrors] = useState("");
+    const [errors, setErrors] = useState(null);
+    const [onSuccess, setOnSuccess] = useState("");
+    const [rerender, setRerender] = useState(false);
+    const [loaded, setLoaded] = useState(false);
     const cookies = new Cookies();
     const access_token = cookies.get("access_token");
-    const updateCategory = async (e) => {
+
+    useEffect(() => {
+      async function getCategories() {
+
+        const params = teacherCourses.map((course) => {
+          return "course_id=" + course.course.id;
+        })
+
         const requestOptions = {
-          method: "PUT",
+          method: "GET",
           headers: {
             "Content-Type": "application/json",
             "Authorization": "Bearer " + access_token,
           },
-          body: JSON.stringify({
-            name: new FormData(e.target).get("categoryName"),
-            id: new FormData(e.target).get("categoryId"),
-          }),
         };
-    
+  
         try {
           const response = await fetch(
-            "http://localhost:8000/updatecategory",
+            "http://localhost:8000/mycoursecategories/?" + params.join('&'),
             requestOptions
           );
-        
+          
           const data = await response.json();
+          let new_object = {};
+          if(response.status == 200) {
+          teacherCourses.forEach((course) => {
+            const new_array = data.filter((category) => { 
+                if (course.course.id === category.course_id) {
+                  return category;
+                }
+              return null;
+            })
+            if(new_array.length > 0) {              
+              new_object[course.course.id] = new_array;
+            }
+         
+          })
+        }
 
+          setCoursesCategories(new_object);
+          setLoaded(true);
         } catch (e) {
           console.log("error:" + e);
         }
-    };
-  
-  const addCategory = async (e) => {
-    const requestOptions = {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "Authorization": "Bearer " + access_token,
-      },
-      body: JSON.stringify({
-        name: new FormData(e.target).get("categoryName"),
-        course_id: new FormData(e.target).get("courseId"),
-      }),
-    };
-  
-    try {
-      const response = await fetch(
-        "http://localhost:8000/addcategory",
-        requestOptions
-      );
-  
-      const data = await response.json();
-      if(response.status !== 201) {
-        setErrors(data.detail)
-        console.log(errors)
       }
-    } catch (e) {
-      console.log("error:" + e);
-    }
+      getCategories();
+    }, [rerender]);  
+    
+  const updateCategory = async (e) => {
+      e.preventDefault()
+      setOnSuccess("");
+      setErrors("");
+      const name = new FormData(e.target).get("categoryName");
+      const id = new FormData(e.target).get("categoryId");
+      if(name.length === 0) {
+        setErrors("Názov nesmie byť prázdny.")
+        return false;
+      }
+      const requestOptions = {
+        method: "PUT",
+        headers: {
+          "Content-Type": "application/json",
+          "Authorization": "Bearer " + access_token,
+        },
+        body: JSON.stringify({
+          name: name,
+          id: id,
+        }),
+      };
+  
+      try {
+        const response = await fetch(
+          "http://localhost:8000/updatecategory",
+          requestOptions
+        );
+      
+        const data = await response.json();
+        
+        if(response.status !== 200) {
+          setErrors(data.detail)
+          console.log("here " + errors)
+        } else {
+          setOnSuccess("Kategória aktualizovaná.")
+          setRerender(!rerender);
+        }
+      } catch (e) {
+        console.log("error:" + e);
+      }
+      document.getElementById("form" + id).reset();
+      return false;
   };
 
+const addCategory = async (e) => {
+  e.preventDefault();
+  setOnSuccess("");
+  setErrors("");
+  const name = new FormData(e.target).get("categoryName");
+  const course_id = new FormData(e.target).get("courseId");
+  
+  if(name.length === 0) {
+    setErrors("Názov nesmie byť prázdny.")
+    return false;
+  }
+
+  const requestOptions = {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+      "Authorization": "Bearer " + access_token,
+    },
+    body: JSON.stringify({
+      name: name,
+      course_id: course_id,
+    }),
+  };
+
+  try {
+    const response = await fetch(
+      "http://localhost:8000/addcategory",
+      requestOptions
+    );
+
+    const data = await response.json();    
+    if(response.status !== 201) {
+      setErrors(data.detail)
+      console.log(errors)
+    } else {
+      setOnSuccess("Kategória pridaná.")
+      setRerender(!rerender);
+    }
+    
+  } catch (e) {
+    console.log("error:" + e);
+  }
+  document.getElementById("formAdd").reset();
+  return false;
+};
+  
     function updateShowCategory(id) {
         if (showCategories === id) {
           setShowCategories(null);
@@ -69,9 +156,11 @@ const CategoryManagement = ({teacherCourses}) => {
           setShowCategories(id);
         }
       }
+
     return( <>
-    {errors && <div>{errors}</div>}
-    {teacherCourses &&
+    {errors && <div className="h4 text-danger">{errors}</div>}
+    {onSuccess && <div className="h4 text-success">{onSuccess}</div>}
+    {loaded && teacherCourses &&
         teacherCourses.map((course, key) => {
           return (
             <>
@@ -95,15 +184,16 @@ const CategoryManagement = ({teacherCourses}) => {
                 </div>
                 {showCategories === course.course.id && (
                   <div className="container" key={key}>
-                    {course.course.categories &&
-                      course.course.categories.map(
+                    {coursesCategories[course.course.id] &&
+                      coursesCategories[course.course.id].map(
                         (category, k) => {
                           return (
                             <>
                               <form
+                              id={"form" + category.id}
                                 className="my-2"
                                 key={k}
-                                onSubmit={(e) => updateCategory(e)}
+                                onSubmit={(e) => (updateCategory(e))}
                               >
                                 <input
                                   type="text"
@@ -111,12 +201,7 @@ const CategoryManagement = ({teacherCourses}) => {
                                   id="categoryName"
                                   placeholder={category.name}
                                 />
-                                <input
-                                  type="hidden"
-                                  id="courseId"
-                                  name="courseId"
-                                  value={category.course_id}
-                                />
+                                
                                 <input
                                   type="hidden"
                                   id="categoryId"
@@ -134,7 +219,7 @@ const CategoryManagement = ({teacherCourses}) => {
                           );
                         }
                       )}
-                    <form onSubmit={(e) => addCategory(e)}>
+                    <form id="formAdd" onSubmit={(e) => addCategory(e)}>
                       <input
                         type="text"
                         name="categoryName"
